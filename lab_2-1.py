3637eb6132b20183fe3d3826d48af13a8b508d5d
P22srogers <p22srogers@fairfieldprep.org>
2021-10-25 09:46:52 -0400

    Submit Cycle 5 Day 2 Labs

---
+++ lab_2-1.py
@@ -0,0 +1,12 @@
+# Author SMR: (AMDG) 10/25/21
+import math
+# 8
+print(math.ceil(7.15))
+# 19
+print(math.floor(19.156))
+# 42
+print(math.fabs(-42))
+# 8
+print(math.pow(2,3))
+# 2 
+print(math.log10(100))
