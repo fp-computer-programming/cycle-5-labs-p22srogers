711dca447a57d783782fd3210f6bea4278790f7a
P22srogers <p22srogers@fairfieldprep.org>
2021-10-26 10:05:21 -0400

    Submit Cycle 5 Day 4 Labs

---
+++ lab_4-1.py
@@ -0,0 +1,8 @@
+# Author: SMR (AMDG) 10/26/21
+print('Fairfield' + ' Prep')
+print('-'*20)
+print("I'm loving this short story I'm reading," ' "The Fall of the House of Usher" ')
+print(len("I'm loving this short story I'm reading," ' "The Fall of the House of Usher" '))
+string="apple"
+character=string[3]
+print(character)
