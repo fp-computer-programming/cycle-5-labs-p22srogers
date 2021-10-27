69000c36f97dd77bc0bd447197f19915be004a8a
P22srogers <p22srogers@fairfieldprep.org>
2021-10-27 10:15:30 -0400

    Submit Cycle 5 Day 5 Labs

---
+++ lab_5-1.py
@@ -0,0 +1,10 @@
+# Author: SMR (AMDG) 10/27/21
+school= "Fairfield Prep"
+first_half= school[0:9]
+print(first_half)
+second_half=school[10:14]
+print(second_half)
+print(first_half)
+new_school= first_half +" " +second_half
+print(new_school)
+
---
+++ lab_5-2.py
@@ -0,0 +1,13 @@
+# Author: SMR (AMDG) 10/27/21
+word1=input("Give me a word: ")
+word2=input("Give me another word: ")
+if word1< word2:
+    print(word1+" comes before " +word2)
+elif word1 >word2:
+    print(word1+" comes after " +word2)
+else:
+    print(word1+" and" +word2+ " are the same")
+
+
+
+
