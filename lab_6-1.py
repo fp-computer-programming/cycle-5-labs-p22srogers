63c93c2a624815d7b6d8d0ab0ba2fc8d0ec2cb29
P22srogers <p22srogers@fairfieldprep.org>
2021-10-29 10:45:52 -0400

    Submit Cycle 5 Day 6 Labs

---
+++ lab_6-1.py
@@ -0,0 +1,5 @@
+# Author: SMR (AMDG) 10/29/21
+gib="flibbertigibbet"
+print(gib.find("t"))
+ss= "I wish, I wish, I was a fish."
+print(ss.split(','))
---
+++ lab_6-2.py
@@ -0,0 +1,5 @@
+# Author: SMR (AMDG) 10/29/21
+first_1=input("User 1 Enter Your First Name: ")
+first_2=input("User 2 Enter Your First Name: ")
+
+print("Hello, {0}. My name is {1}.".format(first_1,first_2))
